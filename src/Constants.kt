83f71c3e23d587ee00a07e982d489508b90f9a18
ianarbuckle <>
2020-08-04 17:37:50 +0100

    Initial commit

---
+++ src/Constants.kt
@@ -0,0 +1,7 @@
+package com.ianarbuckle.conferencesapi
+
+object Constants {
+    const val DATABASE_NAME = "conferences-db"
+    const val COLLECTION_NAME = "conferences"
+    const val COLLECTION_NAME_V2 = "conferences-v2"
+}
